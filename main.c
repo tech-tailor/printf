68fdb37144fc2e6088882337c1f3bcc478f5e192
tech-tailor <ogundareakinniyi8@gmail.com>
2023-04-29 08:58:50 +0000

    changed my main.c

--- main.c
+++ main.c
@@ -20,8 +20,8 @@ ui = (unsigned int)INT_MAX + 1024;
 addr = (void *)0x7ffe637541f0;
 _printf("Length:[%d, %i]\n", len, len);
 printf("Length:[%d, %i]\n", len2, len2);
-_printf("Negative:[%b]\n", 50);
-printf("Negative:[%b]\n", 50);
+_printf("Negative:[%d]\n", -762534);
+printf("Negative:[%d]\n", -762534);
 _printf("Unsigned:[%u]\n", ui);
 printf("Unsigned:[%u]\n", ui);
 _printf("Unsigned octal:[%o]\n", ui);
@@ -40,7 +40,5 @@ _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
 _printf("Unknown:[%r]\n");
 printf("Unknown:[%r]\n");
-len = _printf("%b\n", 98);
-_printf("Len:[%d]\n", len);
 return (0);
 }
